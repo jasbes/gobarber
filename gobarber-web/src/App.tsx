dbc251c09022aa687a1e9ec0ae9d644d3bc2e3b3
Jefferson <jefferson_uk@hotmail.com>
2020-09-02 21:04:40 +0200

    Create login page

---
+++ gobarber-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from './pages/SignIn';
+import GlogalStyle from './styles/global';
+
+const App: React.FC = () => (
+    <>
+      <SignIn />
+      <GlogalStyle />
+    </>
+  );
+
+export default App;
