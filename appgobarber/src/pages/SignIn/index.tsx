20358cfc98edb5602ac482a41c9cf6ab548794ce
Jefferson <jefferson_uk@hotmail.com>
2020-09-22 23:11:29 +0200

    Create login page

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,24 +1,50 @@
 import React from 'react';
-
-import { Image } from 'react-native';
+import {
+  Image, KeyboardAvoidingView, Platform, View, ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logoImg from '../../assets/logo.png';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
-    <Title>Faça seu login</Title>
+  <>
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
+      <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+        <Container>
+          <Image source={logoImg} />
 
-    <Input name="email" icon="mail" placeholder="E-mail" />
+          <View>
+            <Title>Faça seu login</Title>
+          </View>
 
-    <Input name="password" icon="lock" placeholder="Senha" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
 
-    <Button onPress={() => { console.log('deu'); }}>Entrar</Button>
-  </Container>
+          <Input name="password" icon="lock" placeholder="Senha" />
+
+          <Button onPress={() => { console.log('deu'); }}>Entrar</Button>
+
+          <ForgotPassword onPress={() => { console.log('deu'); }}>
+            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+          </ForgotPassword>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+    <CreateAccountButton>
+      <Icon name="log-in" size={20} color="#ff9000" />
+      <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+    </CreateAccountButton>
+  </>
 );
 
 export default SignIn;
