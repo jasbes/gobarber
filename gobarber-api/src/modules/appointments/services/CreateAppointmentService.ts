9cf91291057e0ec92696d8d81f11cdd53e2a0a0f
Jefferson <jefferson_uk@hotmail.com>
2020-09-28 02:05:18 +0200

    Reorganize imports using path

--- gobarber-api/src/modules/appointments/services/CreateAppointmentService.ts
+++ gobarber-api/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,8 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-import Appointment from '../models/Appointment';
-import AppointmentRepository from '../repositories/AppointmentsRepository';
-import AppError from '../errors/AppError';
+import AppError from '@shared/errors/AppError';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppointmentRepository from '@modules/appointments/repositories/AppointmentsRepository';
 
 interface Request {
   provider_id: string;
--- gobarber-api/src/shared/infra/http/server.ts
+++ gobarber-api/src/shared/infra/http/server.ts
@@ -3,11 +3,11 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
-import routes from './shared/routes';
-import uploadConfig from './config/upload';
+import uploadConfig from '@config/upload';
+import routes from '@shared/infra/http/routes';
 
-import './database';
-import AppError from './errors/AppError';
+import '@shared/infra/typeorm';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 app.use(express.json());
