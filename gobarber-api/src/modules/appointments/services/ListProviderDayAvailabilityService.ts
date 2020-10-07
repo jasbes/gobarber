6fcb6ade87385fa71fd2358f54887bab34ec9697
Jefferson <jefferson_uk@hotmail.com>
2020-10-07 11:57:18 +0200

    Implement logic to consider only future dates as available

--- gobarber-api/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ gobarber-api/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
@@ -44,11 +44,19 @@ export default class ListProviderDayAvailabilityService {
       (_, index) => index + hourStart,
     );
 
+    const currentDate = new Date(Date.now());
+
     const availability = hoursArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
-      return { hour, available: !hasAppointmentInHour };
+
+      const compareDate = new Date(year, month - 1, day, hour);
+
+      return {
+        hour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
+      };
     });
 
     return availability;
