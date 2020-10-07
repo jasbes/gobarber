e582cc0ef5463b52e56ca6cf51369d3ab2cdcc35
Jefferson <jefferson_uk@hotmail.com>
2020-10-07 23:12:51 +0200

    Create availability controllers

---
+++ gobarber-api/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProviderDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { year, month, day } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const providerAvailability = await listProviderDayAvailability.execute({
+      provider_id,
+      year,
+      month,
+      day,
+    });
+
+    return response.json(providerAvailability);
+  }
+}
---
+++ gobarber-api/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+
+import ListProviderMonthAvailabilityService from '@modules/appointments/services/ListProviderMonthAvailabilityService';
+
+export default class ProviderMonthAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { year, month } = request.body;
+
+    const listProviderMonthAvailability = container.resolve(
+      ListProviderMonthAvailabilityService,
+    );
+
+    const providerAvailability = await listProviderMonthAvailability.execute({
+      provider_id,
+      year,
+      month,
+    });
+
+    return response.json(providerAvailability);
+  }
+}
